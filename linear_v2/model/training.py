28e6d247efabe321e3363454406ade2076f37f74
Elliott Chartock <elboy@stanford.edu>
2018-06-09 02:43:19 -0700

    forgot period

--- linear_v2/model/training.py
+++ linear_v2/model/training.py
@@ -40,7 +40,7 @@ def train(model, criterion, optimizer, args, dataloader, dataset_size, device):
         y_true += labels.cpu().numpy().squeeze().tolist()
         
         preds = model(inputs)
-        y_pred += preds.cpu()detach().numpy().squeeze().tolist()
+        y_pred += preds.cpu().detach().numpy().squeeze().tolist()
         
         loss = criterion(preds, labels)
         optimizer.zero_grad()
